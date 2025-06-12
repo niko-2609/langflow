6f80f35c0d426eee8617c88302be3e70573a9ae6
nikolai <niko.mak26@gmail.com>
2025-06-12 15:55:36 +0530

    auth complete

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -5,7 +5,7 @@ import prisma from "@/lib/prisma";
 
 
 export async function POST(req: Request) {
-    const secret = process.env.SIGNING_SECRET;
+    const secret = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
     if (!secret) return new Response("Missing secret", { status: 500 });
 
 
@@ -22,15 +22,17 @@ export async function POST(req: Request) {
 
     // Add new users to db when they sign up
     if (event.type === "user.created") {
-        const { id, email_addresses, first_name, last_name, image_url } = event.data;
+        const { id, email_addresses, first_name, last_name, image_url,  } = event.data;
         await prisma.user.upsert({
           where: { clerkId: id },
           update: {},
           create: {
             clerkId: id,
+            id: id,
             email: email_addresses[0].email_address,
-            name: `${first_name} ${last_name}`,
-            image: image_url,
+            firstName: first_name,
+            lastName: last_name,
+            imageUrl: image_url,
           },
         });
       }
